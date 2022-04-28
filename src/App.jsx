4ff511fd86e42e148bb22e4020eecd43b6346235
Charles Michel dos Santos Fernandes <CIANDT\cfernandes@lnb024658cps.ciandt.global>
2022-04-27 21:18:12 -0300

    Configuração das rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from 'paginas/Home'
+import Sobre from 'paginas/Sobre'
 import './assets/css/base/base.css'
 
 function App() {
   return (
-  <>
-
-  </>
+    <BrowserRouter>
+      {/*
+        O Switch previne o funcionamento padrão de ficar procurar outra rotas parecidas
+        caso ache a rota /sobre abre e não faz mais nada
+      */}
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/sobre'>
+          <Sobre />  
+        </Route>
+      </Switch>      
+    </BrowserRouter>
   )
 }
 
